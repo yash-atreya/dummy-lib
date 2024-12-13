c80a4f25248fa76c7b02fbde115cba96d6f6a8f3
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2024-12-13 19:32:10 +0530

    feat: decrement

--- src/Counter.sol
+++ src/Counter.sol
@@ -11,4 +11,10 @@ contract Counter {
     function increment() public {
         number++;
     }
+
+    function decrement() public {
+        if (number > 0) {
+            number--;
+        }
+    }
 }
